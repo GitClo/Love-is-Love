7f169f9f69c49bf31e79bb5b73fda1e39c5b30a1
GitClo <46938275+GitClo@users.noreply.github.com>
2022-02-12 00:07:30 +0000

    Add files via upload

---
+++ iloveyoubaby.py
@@ -0,0 +1,28 @@
+#iloveallcreatures it's just how it is baby omgomgomg
+#im so desperate that any human interaction would make me happy :D:D:D:D:D:D:D:D :|
+
+import webbrowser
+
+w = "h"
+o = ".com"
+u = "ube"
+l = "tch"
+d = "wa"
+
+y = "t"
+ou = "?"
+
+p = "ps"
+le = "/"
+a = "v="
+s = "dQw4w9WgXcQ"
+e = "://"
+
+re = "w"
+ply = "you"
+
+love = w+y+y+p+e+re+re+re+"."+ply+y+u+o+le+d+l+ou+a+s
+webbrowser.open(love)
+
+#This thrash was made by THE ServerFuhrer aka GitClo aka penetrator of you mother ahahaha insemination is my passion
+#No really... i'm starting to fell like i need to breathe some freash air after a week :)
